55da1a7235fd177d00d838887fe4c5c293e0da6d
Haironch <chavezhairon360@gmail.com>
2025-05-14 20:53:52 -0600

    Se modifico las variables de entonro

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -1,6 +1,6 @@
-// firebase/firebase.config.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./clave-firebase.json");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
